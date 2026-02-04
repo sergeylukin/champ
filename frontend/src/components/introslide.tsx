90721330d16dbfcb38b1593151febc3c2b1a6d46
Sergey Lukin <contact@sergeylukin.com>
2026-02-04 21:26:10 +0200

    update intro screen look&feel

--- frontend/src/components/introslide.tsx
+++ frontend/src/components/introslide.tsx
@@ -6,17 +6,7 @@ export const IntroSlide = () => {
   return (
     visible && (
       <div className="Intro absolute h-full w-full z-10">
-        <div className="Intro-title py-4">
-          <p className="text-3xl text-secondary">
-            Take PART: Participation Activities Rating for Teenagers
-          </p>
-          <p className="text-2xl mt-4 font-bold">
-            אבחון דירוג פעילות והשתתפות לבני נוער
-          </p>
-          <p className="text-l mt-4 text-secondary"></p>
-          <p className="text-l mt-1 text-secondary">
-            מרב פורת, אפרת לנדס, ד״ר לירון לאמאש, ד״ר יעל פוגל
-          </p>
+        <div className="Intro-title pb-4">
           <Button
             type="submit"
             className="mx-auto w-[200px] mt-5"
