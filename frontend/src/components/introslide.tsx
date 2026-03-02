7d75057bb08efc38c99cef2ad261bac86f333a34
Yotam <yotamizakov@gmail.com>
2026-03-02 22:29:59 +0200

    intro page

--- frontend/src/components/introslide.tsx
+++ frontend/src/components/introslide.tsx
@@ -6,10 +6,13 @@ export const IntroSlide = () => {
   return (
     visible && (
       <div className="Intro absolute h-full w-full z-10">
-        <div className="Intro-title pb-4">
+        <div className="w-full h-full flex flex-col items-center justify-center">
+          <img src="/intro-title.jpg"
+          width="auto" height="auto"
+          className="mx-auto left-0 right-0 w-[40vw] h-auto" />
           <Button
             type="submit"
-            className="mx-auto w-[200px] mt-5 !bg-[#8591a1] !text-[#d1d8e0]"
+            className="mx-auto w-[30vw] py-[30px] mt-5 !bg-[#8591a1] !text-[#d1d8e0]"
             onClick={() => setVisible(false)}
           >
             {"התחל"}
